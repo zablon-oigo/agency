7fc92b3cf1a6d8ce3f28ca1934b9d70e79f56c6b
zablon <zablonoigo51@gmail.com>
2024-03-08 09:46:51 +0300

    adding login and signup button

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import logo from '../assets/image/logo.png'
 function Navbar() {
   return (
    <header className="w-full bg-white md:bg-transparent fixed top-0 left-0 right-0">
-    <nav className="flex items-center justify-between">
+    <nav className="flex items-center justify-between px-10 py-6">
         <div className="">
             <a href="" className="flex items-center space-x-2 ">
                 <img src={logo} alt="" className='inline-block items-center w-10 h-10' />
@@ -13,7 +13,7 @@ function Navbar() {
                
             </a>
             </div>
-            <div className="flex items-center justify-center space-x-6">
+            <div className="hidden md:flex items-center justify-center space-x-6">
                 <a href="" className="text-gray-900 hover:text-brandPrimary text-base font-medium ">home</a>
                 <a href="" className="text-gray-900 hover:text-brandPrimary text-base font-medium ">services</a>
                 <a href="" className="text-gray-900 hover:text-brandPrimary text-base font-medium ">about</a>
@@ -23,6 +23,14 @@ function Navbar() {
 
 
                 </div>
+            <div className="md:block hidden space-x-6">
+                <a href="" className="capitalize text-brandPrimary hover:text-gray-900">
+                    login
+                </a>
+                <button className="bg-brandPrimary text-white py-2 px-4 transition-all duration-300 rounded hover:bg-neutralDGray">
+                    sign up
+                </button>
+            </div>
     </nav>
    </header>
   )
