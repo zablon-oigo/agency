8153c19be121be6d2c9bf48004e65645a5607f17
zablon <zablonoigo51@gmail.com>
2024-03-08 08:01:03 +0300

    adding default colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,14 @@ export default {
 "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        'neutralSilver':'#F5F7FA',
+        'neutralDGray':'#4O4D4O',
+        'brandPrimary':'#4CAF4F',
+        'neutralGray':'#717171',
+      }
+    },
   },
   plugins: [],
 }
