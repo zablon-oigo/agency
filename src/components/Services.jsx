cfa9c7a5e7a95da40ec543bc8787af5f4915831a
zablon <zablonoigo51@gmail.com>
2024-03-10 15:44:53 +0300

    adding services heading

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -2,7 +2,16 @@ import React from 'react'
 
 function Services() {
   return (
-    <div>Services</div>
+    <div className='md:px-14 px-4 py-16 max-w-screen-2xl mx-auto'>
+        <div className="text-center my-8">
+            <h1 className="text-4xl text-neutralDGray font-semibold mb-2">
+                our clients
+            </h1>
+            <p className="text-neutralGray">
+               Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate, eius ut. Labore sunt iure dolor praesentium!
+            </p>
+        </div>
+    </div>
   )
 }
 
