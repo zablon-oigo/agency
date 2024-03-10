1148a5968a45c2f140bb226fa567b0b4d46afb35
zablon <zablonoigo51@gmail.com>
2024-03-10 15:22:36 +0300

    adding Carousel

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,19 +4,65 @@ function Header() {
   return (
     <div className='bg-neutralSilver'>
         <div className="px-4 md:px-14 max-w-screen-2xl mx-auto min-h-screen h-screen">
-        <div className="h-56 sm:h-64 xl:h-80 2xl:h-96">
-      <Carousel onSlideChange={(index) => console.log('onSlideChange()', index)}>
-        <div className="flex h-full items-center justify-center bg-gray-400 dark:bg-gray-700 dark:text-white">
-          Slide 1
+        
+      <Carousel className='w-full mx-auto'>
+        <div className="my-28 md:my-8 py-12 flex flex-col md:flex-row-reverse justify-between items-center gap-12">
+          <div className="">
+          <img src="" alt="" className="" />
+          </div>
+          <div className="md:w-1/2">
+            <h1 className="text-5xl font-semibold mb-4 text-neutralDGray md:w-3/4 leading-snug">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita atque recusandae <span className="text-brandPrimary">
+            officiis dolores. Architecto
+            </span>
+            </h1>
+            <p className="text-neutralDGray text-base mb-8 ">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita atque recusandae officiis dolores. Architecto, inventore at?
+            </p>
+              <button className="px-7 py-2 bg-brandPrimary text-white rounded hover:bg-neutralDGray transition-all duration-300 hover:-translate-y-4">
+                Register
+              </button>
+          </div>
         </div>
-        <div className="flex h-full items-center justify-center bg-gray-400 dark:bg-gray-700 dark:text-white">
-          Slide 2
+        <div className="my-28 md:my-8 py-12 flex flex-col md:flex-row-reverse justify-between items-center gap-12">
+          <div className="">
+          <img src="" alt="" className="" />
+          </div>
+          <div className="md:w-1/2">
+            <h1 className="text-5xl font-semibold mb-4 text-neutralDGray md:w-3/4 leading-snug">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita atque recusandae <span className="text-brandPrimary">
+            officiis dolores. Architecto
+            </span>
+            </h1>
+            <p className="text-neutralDGray text-base mb-8 ">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita atque recusandae officiis dolores. Architecto, inventore at?
+            </p>
+              <button className="px-7 py-2 bg-brandPrimary text-white rounded hover:bg-neutralDGray transition-all duration-300 hover:-translate-y-4">
+                Register
+              </button>
+          </div>
         </div>
-        <div className="flex h-full items-center justify-center bg-gray-400 dark:bg-gray-700 dark:text-white">
-          Slide 3
+        <div className="my-28 md:my-8 py-12 flex flex-col md:flex-row-reverse justify-between items-center gap-12">
+          <div className="">
+          <img src="" alt="" className="" />
+          </div>
+          <div className="md:w-1/2">
+            <h1 className="text-5xl font-semibold mb-4 text-neutralDGray md:w-3/4 leading-snug">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita atque recusandae <span className="text-brandPrimary">
+            officiis dolores. Architecto
+            </span>
+            </h1>
+            <p className="text-neutralDGray text-base mb-8 ">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita atque recusandae officiis dolores. Architecto, inventore at?
+            </p>
+              <button className="px-7 py-2 bg-brandPrimary text-white rounded hover:bg-neutralDGray transition-all duration-300 hover:-translate-y-4">
+                Register
+              </button>
+          </div>
         </div>
+        
       </Carousel>
-    </div>
+   
         </div>
     </div>
   )
