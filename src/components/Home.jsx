e1582fe26c49b0861cfa36ce311217e51a0d1f00
zablon <zablonoigo51@gmail.com>
2024-03-10 15:36:48 +0300

    adding services component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Navbar from './Navbar'
 import Header from './Header'
+import Services from './Services'
 function Home() {
   return (
     <>
     <Navbar/>
     <Header/>
+    <Services/>
     </>
   )
 }
