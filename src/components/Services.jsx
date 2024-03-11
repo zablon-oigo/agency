763b3e5ec46935e6015b21678cf46c639b6034db
zablon <zablonoigo51@gmail.com>
2024-03-11 16:51:54 +0300

    adding min height screen

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Services() {
   return (
-    <div className='md:px-14 px-4 py-16 max-w-screen-2xl mx-auto'>
+    <div className='md:px-14 px-4 py-16 max-w-screen-2xl min-h-[480px] mx-auto'>
         <div className="text-center my-8">
             <h1 className="text-4xl text-neutralDGray font-semibold mb-2">
                 our clients
