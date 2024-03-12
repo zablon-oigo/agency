5be588a0c0f8629737c9cf9e66e420734385c182
zablon <zablonoigo51@gmail.com>
2024-03-12 21:46:02 +0300

    adding about and footer component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,12 +2,15 @@ import React from 'react'
 import Navbar from './Navbar'
 import Header from './Header'
 import Services from './Services'
+import About from './About'
 function Home() {
   return (
     <>
     <Navbar/>
     <Header/>
     <Services/>
+    <About/>
+    <Footer/>
     </>
   )
 }
