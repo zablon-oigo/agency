fb6176b5e8d8691067d307adb470725dc7f70236
zablon <zablonoigo51@gmail.com>
2024-03-13 09:10:46 +0300

    Changed title width on large screen

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -11,7 +11,25 @@ function Header() {
           <img src="" alt="" className="" />
           </div>
           <div className="md:w-1/2">
-            <h1 className="text-5xl font-semibold mb-4 text-neutralDGray md:w-3/4 leading-snug">
+            <h1 className="text-5xl font-semibold mb-4 text-neutralDGray  leading-snug">
+            Lorem ipsum dolor sit amet consectetur  <span className="text-brandPrimary">
+            officiis dolores. Architecto
+            </span>
+            </h1>
+            <p className="text-neutralDGray text-base mb-8 ">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita atque recusandae officiis dolores. Architecto, inventore at?
+            </p>
+              <button className="px-7 py-2 bg-brandPrimary text-white rounded hover:bg-neutralDGray transition-all duration-300 hover:-translate-y-4">
+                Register
+              </button>
+          </div>
+        </div>
+        <div className="my-28 md:my-8 py-12 flex flex-col md:flex-row-reverse justify-between items-center gap-12">
+          <div className="">
+          <img src="" alt="" className="" />
+          </div>
+          <div className="md:w-1/2">
+            <h1 className="text-5xl font-semibold mb-4 text-neutralDGray  leading-snug">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita atque recusandae <span className="text-brandPrimary">
             officiis dolores. Architecto
             </span>
@@ -29,25 +47,7 @@ function Header() {
           <img src="" alt="" className="" />
           </div>
           <div className="md:w-1/2">
-            <h1 className="text-5xl font-semibold mb-4 text-neutralDGray md:w-3/4 leading-snug">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita atque recusandae <span className="text-brandPrimary">
-            officiis dolores. Architecto
-            </span>
-            </h1>
-            <p className="text-neutralDGray text-base mb-8 ">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita atque recusandae officiis dolores. Architecto, inventore at?
-            </p>
-              <button className="px-7 py-2 bg-brandPrimary text-white rounded hover:bg-neutralDGray transition-all duration-300 hover:-translate-y-4">
-                Register
-              </button>
-          </div>
-        </div>
-        <div className="my-28 md:my-8 py-12 flex flex-col md:flex-row-reverse justify-between items-center gap-12">
-          <div className="">
-          <img src="" alt="" className="" />
-          </div>
-          <div className="md:w-1/2">
-            <h1 className="text-5xl font-semibold mb-4 text-neutralDGray md:w-3/4 leading-snug">
+            <h1 className="text-5xl font-semibold mb-4 text-neutralDGray  leading-snug">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita atque recusandae <span className="text-brandPrimary">
             officiis dolores. Architecto
             </span>
