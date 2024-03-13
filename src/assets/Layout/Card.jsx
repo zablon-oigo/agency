b72778acdb2b8cc2f5bf3e6e0549d615d6e10362
zablon <zablonoigo51@gmail.com>
2024-03-13 09:24:45 +0300

    adding card items using map function

--- src/assets/Layout/Card.jsx
+++ src/assets/Layout/Card.jsx
@@ -1,9 +1,46 @@
-import React from 'react'
+import React from 'react';
 
 function Card() {
+  const items = [
+    {
+      id: 1,
+      title: 'lorem ipsum',
+      icon: '',
+      description: 'lorem ipsum dolor sit amet'
+    },
+    {
+      id: 2,
+      title: 'lorem ipsum',
+      icon: '',
+      description: 'lorem ipsum dolor sit amet'
+    },
+    {
+      id: 3,
+      title: 'lorem ipsum',
+      icon: '',
+      description: 'lorem ipsum dolor sit amet'
+    }
+  ];
+
   return (
-    <div>Card</div>
-  )
+    <div className='flex items-center justify-between gap-10'>
+      {items.map((item) => (
+        <div className="" key={item.id}>
+          <div className="">
+            <span className="">
+              {item.icon}
+            </span>
+          </div>
+          <h2 className="">
+            {item.title}
+          </h2>
+          <p className="">
+            {item.description}
+          </p>
+        </div>
+      ))}
+    </div>
+  );
 }
 
-export default Card
+export default Card;
