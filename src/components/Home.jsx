a341fcbc548cfe2f4e038ab8d2eeae6a650dbdcc
zablon <zablonoigo51@gmail.com>
2024-03-13 09:01:32 +0300

    adding footer component path

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,6 +3,7 @@ import Navbar from './Navbar'
 import Header from './Header'
 import Services from './Services'
 import About from './About'
+import Footer from './Footer'
 function Home() {
   return (
     <>
