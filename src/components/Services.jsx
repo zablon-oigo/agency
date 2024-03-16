6fb39eb41b6aabf480389e165e89abf41b9f6cd0
zablon <zablonoigo51@gmail.com>
2024-03-16 16:13:03 +0300

    aligning services icon on same line

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,5 +1,11 @@
 import React from 'react'
 import Card from '../assets/Layout/Card'
+import img1 from '../assets/image/services/img1.png'
+import img2 from '../assets/image/services/img2.png'
+import img3 from '../assets/image/services/img3.png'
+import img4 from '../assets/image/services/img4.png'
+import img5 from '../assets/image/services/img5.png'
+import img6 from '../assets/image/services/img6.png'
 function Services() {
   return (
     <div className='md:px-14 px-4 py-16 max-w-screen-2xl min-h-[480px] mx-auto'>
@@ -11,12 +17,14 @@ function Services() {
                Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate, eius ut. Labore sunt iure dolor praesentium!
             </p>
         </div>
-        <div className="my-20">
-          <div className="flex justify-center items-center">
-            <img src="" alt="" className="" />
-            <img src="" alt="" className="" />
-            <img src="" alt="" className="" />
-
+        <div className="my-10">
+          <div className="flex justify-between items-center">
+            <img src={img1} alt="" className="w-12 h-12" />
+            <img src={img2} alt="" className="w-12 h-12" />
+            <img src={img3} alt="" className="w-12 h-12" />
+            <img src={img4} alt="" className="w-12 h-12" />
+            <img src={img5} alt="" className="w-12 h-12" />
+            <img src={img6} alt="" className="w-12 h-12" />
           </div>
           <div className="my-10 text-center">
             <h2 className="first-letter:uppercase font-semibold text-3xl text-neutralDGray">
