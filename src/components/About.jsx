71d86347fae68242861ab6794051192d2cc64025
zablon <zablonoigo51@gmail.com>
2024-03-16 14:49:49 +0300

    about component

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,7 +2,21 @@ import React from 'react'
 
 function About() {
   return (
-    <div>About</div>
+    <div className='min-h-[400px] bg-gray-100'>
+      <div className="md:py-40 px-10 py-20 flex items-center md:flex-row flex-col justify-between">
+        <div className="md:w-1/2 w-full">
+          <img src="" alt="" className="" />
+        </div>
+        <div className="md:w-1/2 w-full">
+          <h2 className="text-center md:text-start text-3xl mb-6 capitalize">
+            lorem ipsum dolor
+          </h2>
+          <p className="text-base md:text-center text-start text-neutralDGray first-letter:capitalize ">
+           Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore qui sed, voluptatem amet in sint ratione labore reprehenderit earum? Consectetur corporis expedita a? Odio debitis, nemo adipisci quo culpa eum, neque ex aliquid, minus id blanditiis enim. Molestias, quibusdam accusamus.
+          </p>
+        </div>
+      </div>
+    </div>
   )
 }
 
