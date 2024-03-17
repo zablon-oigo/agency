e9244bb6bbff2b8f830aaacc2d209b8f81e5aea8
zablon <zablonoigo51@gmail.com>
2024-03-17 11:15:17 +0300

    adding image icon

--- src/assets/Layout/Card.jsx
+++ src/assets/Layout/Card.jsx
@@ -1,34 +1,37 @@
 import React from 'react';
+import icon1 from '../image/icon/icon1.png'
+import icon2 from '../image/icon/icon2.png'
+import icon3 from '../image/icon/icon3.png'
 
 function Card() {
   const items = [
     {
       id: 1,
       title: 'lorem ipsum',
-      icon: '',
+      icon: icon1,
       description: 'lorem ipsum dolor sit amet'
     },
     {
       id: 2,
       title: 'lorem ipsum',
-      icon: '',
+      icon: icon2,
       description: 'lorem ipsum dolor sit amet'
     },
     {
       id: 3,
       title: 'lorem ipsum',
-      icon: '',
+      icon: icon3,
       description: 'lorem ipsum dolor sit amet'
     }
   ];
 
   return (
-    <div className='flex items-center justify-between gap-10'>
+    <div className='flex md:flex-row flex-col-reverse items-center justify-between gap-10'>
       {items.map((item) => (
         <div className="" key={item.id}>
           <div className="">
             <span className="">
-              {item.icon}
+              <img src={item.icon} alt="" className='w-12 h-12' />
             </span>
           </div>
           <h2 className="">
