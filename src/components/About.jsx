628233e59e7dc9b955972283f5be116063652da9
zablon <zablonoigo51@gmail.com>
2024-03-17 14:44:04 +0300

    reduced min height on about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
-
+import about from '../assets/image/about.png'
 function About() {
   return (
-    <div className='min-h-[400px] bg-gray-100'>
-      <div className="md:py-40 px-10 py-20 flex items-center md:flex-row flex-col justify-between">
+    <div className='min-h-[200px] bg-gray-100'>
+      <div className=" px-10 py-20 flex items-center md:flex-row flex-col justify-between">
         <div className="md:w-1/2 w-full">
-          <img src="" alt="" className="" />
+          <img src={about} alt="" className="" />
         </div>
         <div className="md:w-1/2 w-full">
-          <h2 className="text-center md:text-start text-3xl mb-6 capitalize">
-            lorem ipsum dolor
+          <h2 className="text-center md:text-start first-letter:capitalize text-3xl mb-6 capitalize">
+           the unseen of spending three years at Pixelgrade
           </h2>
           <p className="text-base text-center md:text-start text-neutralDGray first-letter:capitalize ">
            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore qui sed, voluptatem amet in sint ratione labore reprehenderit earum? Consectetur corporis expedita a? Odio debitis, nemo adipisci quo culpa eum, neque ex aliquid, minus id blanditiis enim. Molestias, quibusdam accusamus.
