835dab34e6a5afca080c4b1ac47b1371dda28d22
zablon <zablonoigo51@gmail.com>
2024-03-18 15:53:41 +0300

    adding button translation on y axis on hover

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 import about from '../assets/image/about.png'
 function About() {
   return (
-    <div className='min-h-[200px]'>
-      <div className=" px-10 py-20 flex items-center md:flex-row flex-col justify-between">
+    <div className='bg-gray-50'>
+      <div className=" px-10 py-10 flex items-center md:flex-row flex-col justify-between">
         <div className="md:w-1/2 w-full">
           <img src={about} alt="" className="" />
         </div>
@@ -15,7 +15,7 @@ function About() {
            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore qui sed, voluptatem amet in sint ratione labore reprehenderit earum? Consectetur corporis expedita a? Odio debitis, nemo adipisci quo culpa eum, neque ex aliquid, minus id blanditiis enim. Molestias, quibusdam accusamus.
           </p>
           <div className="flex justify-center md:justify-start">
-            <button className="px-6 py-3 text-white mt-6 rounded capitalize bg-brandPrimary">
+            <button className="px-6 py-3 text-white mt-6 rounded capitalize bg-brandPrimary hover:bg-neutralDGray hover:text-white hover:-translate-y-2 duration-300 transition-transform">
               learn more
             </button>
           </div>
