7323942c667af78ee5ab813e40d5b64b7d459bdc
zablon <zablonoigo51@gmail.com>
2024-03-19 11:00:05 +0300

    Reducing font size

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -8,15 +8,15 @@ function Product() {
             <div className="">
             <img src={product} alt="" />
             </div>
-            <div className="text-center">
-                <h2 className="text-3xl capitalize mb-4 font-bold">
-                    how to design your footer like we did 
+            <div className="">
+                <h2 className="md:text-3xl text-xl capitalize mb-4 text-center md:text-start font-bold">
+                    how to design your <span className="text-brandPrimary">footer</span> like we did 
                 </h2>
-                <p className="text-base  first-letter:capitalize text-neutralDGray mb-4">
+                <p className="md:text-base text-sm  text-center md:text-start font-light first-letter:capitalize text-neutralDGray mb-4">
                     Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quaerat ut, deleniti atque nam adipisci ex architecto minus itaque obcaecati in ipsum laborum accusamus saepe asperiores cumque doloribus illo cupiditate ratione assumenda expedita tenetur ipsam soluta quas. Nulla praesentium quibusdam explicabo.
                 </p>
-                <div className="">
-                    <button className="px-6 py-3 rounded-lg bg-brandPrimary text-white capitalize transition-all duration-300 hover:bg-neutralDGray hover:-translate-y-2">
+                <div className="flex md:justify-start justify-center">
+                    <button className="px-6 py-3 rounded bg-brandPrimary text-white capitalize transition-all duration-300 hover:bg-neutralDGray hover:-translate-y-2">
                         learn more
                     </button>
                 </div>
