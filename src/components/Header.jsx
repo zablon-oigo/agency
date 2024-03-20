afa59ddea0d0d6f7a546776e7fec8d60401ec9b5
zablon <zablonoigo51@gmail.com>
2024-03-20 08:05:46 +0300

    reducing margin on y axis

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,24 +5,24 @@ import head2 from '../assets/image/about.png'
 function Header() {
   return (
     <div className='bg-neutralSilver'>
-        <div className="px-4 md:px-14 max-w-screen-2xl mx-auto min-h-screen h-screen ">
+        <div className="px-4 mx-auto  md:px-14 max-w-screen-2xl">
         
-      <Carousel className='w-full mx-auto -my-80 md:my-0 '>
-        <div className="  flex flex-col md:flex-row-reverse justify-between items-center gap-12">
+      <Carousel className='w-full mx-auto md:my-0 '>
+        <div className="flex flex-col items-center justify-between gap-12 md:flex-row-reverse">
           <div className="">
           <img src={head1} alt="" className="" />
           </div>
           <div className="md:w-1/2">
-            <h1 className="capitalize md:text-5xl text-3xl font-semibold mb-4 text-neutralDGray md:text-start text-center  leading-snug">
+            <h1 className="mb-4 text-3xl font-semibold leading-snug text-center capitalize md:text-5xl text-neutralDGray md:text-start">
             lessons and insight  <span className="text-brandPrimary">
             from 8 years
             </span>
             </h1>
-            <p className=" first-letter:capitalize text-neutralDGray md:text-base text-md mb-8 text-center md:text-start ">
+            <p className="mb-8 text-center first-letter:capitalize text-neutralDGray md:text-base text-md md:text-start">
               where to grow your business as a photographer:site or social media ?
             </p>
             <div className="flex items-center justify-center md:justify-start">
-            <button className="px-7 py-2 bg-brandPrimary text-white rounded hover:bg-neutralDGray transition-all duration-300 hover:-translate-y-4 ">
+            <button className="py-2 text-white transition-all duration-300 rounded px-7 bg-brandPrimary hover:bg-neutralDGray hover:-translate-y-4 ">
                 Register
               </button>
             </div>
@@ -30,21 +30,21 @@ function Header() {
           </div>
         </div>
         
-        <div className="  flex flex-col md:flex-row-reverse justify-between items-center gap-12">
+        <div className="flex flex-col items-center justify-between gap-12 md:flex-row-reverse">
           <div className="">
           <img src={head2} alt="" className="" />
           </div>
           <div className="md:w-1/2">
-            <h1 className="capitalize md:text-5xl text-3xl font-semibold mb-4 text-neutralDGray md:text-start text-center  leading-snug">
+            <h1 className="mb-4 text-3xl font-semibold leading-snug text-center capitalize md:text-5xl text-neutralDGray md:text-start">
             learn design and illustrations  <span className="text-brandPrimary">
             in 4 months
             </span>
             </h1>
-            <p className="first-letter:capitalize text-neutralDGray md:text-base text-md mb-8 text-center md:text-start ">
+            <p className="mb-8 text-center first-letter:capitalize text-neutralDGray md:text-base text-md md:text-start ">
               where to grow your business as a photographer:site or social media ?
             </p>
             <div className="flex items-center justify-center md:justify-start">
-            <button className="px-7 py-2 bg-brandPrimary text-white rounded hover:bg-neutralDGray transition-all duration-300 hover:-translate-y-4 ">
+            <button className="py-2 text-white transition-all duration-300 rounded px-7 bg-brandPrimary hover:bg-neutralDGray hover:-translate-y-4 ">
                 Register
               </button>
             </div>
@@ -52,21 +52,21 @@ function Header() {
           </div>
         </div>
         
-        <div className="  flex flex-col md:flex-row-reverse justify-between items-center gap-12">
+        <div className="flex flex-col items-center justify-between gap-12 md:flex-row-reverse">
           <div className="">
           <img src={head1} alt="" className="" />
           </div>
           <div className="md:w-1/2">
-            <h1 className="md:text-5xl text-3xl font-semibold mb-4 text-neutralDGray md:text-start text-center  leading-snug">
+            <h1 className="mb-4 text-3xl font-semibold leading-snug text-center md:text-5xl text-neutralDGray md:text-start">
             lessons and insight  <span className="text-brandPrimary">
             from 8 years
             </span>
             </h1>
-            <p className="text-neutralDGray md:text-base text-md mb-8 text-center md:text-start ">
+            <p className="mb-8 text-center text-neutralDGray md:text-base text-md md:text-start ">
               where to grow your business as a photographer:site or social media ?
             </p>
             <div className="flex items-center justify-center md:justify-start">
-            <button className="px-7 py-2 bg-brandPrimary text-white rounded hover:bg-neutralDGray transition-all duration-300 hover:-translate-y-4 ">
+            <button className="py-2 text-white transition-all duration-300 rounded px-7 bg-brandPrimary hover:bg-neutralDGray hover:-translate-y-4 ">
                 Register
               </button>
             </div>
