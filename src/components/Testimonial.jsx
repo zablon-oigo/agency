65333b45440a35eff5becc250d7aaf9e069c99f9
zablon <zablonoigo51@gmail.com>
2024-03-21 08:57:24 +0300

    Reducing Font size on smaller screen

--- src/components/Testimonial.jsx
+++ src/components/Testimonial.jsx
@@ -6,10 +6,10 @@ function Testimonial() {
     <>
     <div className="min-h-[200px] bg-slate-50 px-10 py-16">
         <div className="flex flex-col items-center justify-center">
-            <h2 className="text-3xl font-bold text-center capitalize text-neutral-600">
+            <h2 className="text-xl font-bold text-center capitalize md:text-3xl text-neutral-600">
                 caring is the new marketing
             </h2>
-            <p className="w-2/5 mt-4 font-light text-center text-neutralDGray first-letter:capitalize">
+            <p className="w-full mt-4 text-sm font-light text-center md:text-base md:w-2/5 text-neutralDGray first-letter:capitalize">
                 the nextcent blog is the best place to read about the latest membership insights, 
                 trends and more.See who's joining the community, read about how our community are increasing their membership income and lot's more
             </p>
