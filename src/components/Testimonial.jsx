37e53f62c7d5474467d166b593c6fa2cf85c88cd
zablon <zablonoigo51@gmail.com>
2024-03-21 08:29:08 +0300

    Adding min height screen

--- src/components/Testimonial.jsx
+++ src/components/Testimonial.jsx
@@ -3,7 +3,17 @@ import React from 'react'
 function Testimonial() {
   return (
     <>
-    Testimonial
+    <div className="min-h-[200px] bg-slate-50 px-10 py-16">
+        <div className="">
+            <h2 className="">
+                caring is the new marketing
+            </h2>
+            <p className="">
+                the nextcent blog is the best place to read about the latest membership insights, 
+                trends and more.See who's joining the community, read about how our community are increasing their membership income and lot's more
+            </p>
+        </div>
+    </div>
     </>
   )
 }
