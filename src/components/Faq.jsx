79d1507d876528eadd18695296dad504ccae701d
zablon <zablonoigo51@gmail.com>
2024-03-22 10:58:31 +0300

    Changed font size on smaller screen

--- src/components/Faq.jsx
+++ src/components/Faq.jsx
@@ -5,7 +5,7 @@ function Faq() {
     <>
     <div className="min-h-[250px] bg-gradient-to-br from-[#F0EAD6] to-[#E9DCC9] md:px-10 px-2 py-10">
         <div className="flex flex-col items-center justify-center w-full">
-            <h2 className="text-3xl font-semibold text-center capitalize md:w-2/5 ">
+            <h2 className="text-xl font-semibold text-center capitalize md:text-3xl md:w-2/5 ">
                 Lorem ipsum, dolor sit amet consectetur adipisicing elit. Libero, eaque.
             </h2>
             <div className="my-4">
