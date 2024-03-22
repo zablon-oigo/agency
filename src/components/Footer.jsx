162aceab9fb956f60e46d47bf37ac209fa7cb57b
zablon <zablonoigo51@gmail.com>
2024-03-22 11:32:36 +0300

    Adding Flex box

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,16 @@ import React from 'react'
 function Footer() {
   return (
     <>
-    <div className="min-h-[300px] bg-[#263238] "></div>
+    <div className="min-h-[300px] bg-[#263238] ">
+      <div className="flex flex-col justify-center px-10 py-10 md:flex-row">
+        <div className="">
+          
+        </div>
+        <div className="">
+
+        </div>
+      </div>
+    </div>
     </>
   )
 }
