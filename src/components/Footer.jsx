ffb3b1398f036e2712a5dd7af7f71b276388ce75
zablon <zablonoigo51@gmail.com>
2024-03-23 14:35:39 +0300

    Adding flex box on grid container

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,8 +9,8 @@ function Footer() {
   return (
     <>
     <div className="min-h-[300px] bg-[#263238] ">
-      <div className="flex flex-col justify-center px-10 py-10 md:flex-row">
-        <div className="">
+      <div className="flex flex-col justify-between px-10 py-10 md:flex-row">
+        <div className="w-full md:w-2/5">
             <Link to={'/'} smooth={true} spy={true} duration={500} className="flex items-center space-x-2 cursor-pointer ">
                     <img src={logo} alt="" className='items-center inline-block w-6 h-6' />
                     <span className="text-[#FAF9F6] font-bold text-2xl uppercase">
@@ -30,8 +30,25 @@ function Footer() {
                 <TbWorld className='text-[#E2DFD2] text-3xl '/>
                 </div>
         </div>
-        <div className="">
-
+        <div className="flex justify-around w-full md:w-3/4">
+             <div className="">
+             <h2 className="text-xl capitalize font-semibold  text-[#FFF5EE]">
+              company
+             </h2>
+             </div>
+             <div className="">
+              <h2 className="text-xl capitalize font-semibold  text-[#FFF5EE]">
+                support
+              </h2>
+             </div>
+             <div className="">
+              <h2 className="text-xl capitalize font-semibold  text-[#FFF5EE]">
+                stay up to date
+              </h2>
+              <div className="">
+                <input type="text" className="" />
+              </div>
+             </div>
         </div>
       </div>
     </div>
